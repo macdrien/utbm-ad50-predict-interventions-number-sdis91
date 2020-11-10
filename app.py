f9db5e7f128f200bfad51252a209021fe77f121c
abouysso <adrien.bouyssou@utbm.fr>
2020-11-10 21:03:15 +0100

    [ADD] Load and filter incidence-RDD-... files
    Create a function which do the following:
    
    - Load the panda's DataFrame from the CSV (identified by a filepath)
    - Iterate on each row to keep only those who match with the given lambda
    function
    - Create and return a new DataFrame from the filtered data
    
    ---
    
    Use the created function to load and filter all incidence-RDD files.
    Add the temporary print to show that the result is the wanted one.

---
+++ app.py
@@ -0,0 +1,33 @@
+import pandas
+import numpy
+
+datadir = './datas/'
+sdis91_filepath = datadir + 'interventions-hebdo-2010-2017.csv'
+gripe_filepath = datadir + 'incidence-RDD-3-syndromes-grippaux.csv'
+diarrhee_filepath = datadir + 'incidence-RDD-6-syndrome-diarrhee-aigue.csv'
+varicelle_filepath = datadir + 'incidence-RDD-7-varicelle.csv'
+days_off_filepath = datadir + 'hollidays_and_days_off.csv'
+
+
+def load_and_filter_incidence_rdd_file(filepath, row_validator):
+    """
+    Load data from a csv incidence-RDD-... (could function with other CSV but the function is written for this files)
+    Filter the got data using the function row_validator
+
+    :param filepath: string -  The path to the file containing the wanted data.
+    :param row_validator: function - Function returning a Boolean. It will be used to filter which rows will be kept
+
+    :return: A pandas.DataFrame containing the data of filepath filtered by row_validator
+    """
+    data = pandas.read_csv(filepath)
+    filtered_data = [row[1] for row in data.iterrows() if row_validator(row)]
+    return pandas.DataFrame(numpy.array(filtered_data), columns= data.columns)
+
+filter_ile_de_france = lambda row: row[1]['geo_name'] == 'ILE-DE-FRANCE'
+gripe_data = load_and_filter_incidence_rdd_file(gripe_filepath, filter_ile_de_france)
+diarrhee_data = load_and_filter_incidence_rdd_file(diarrhee_filepath, filter_ile_de_france)
+varicelle_data = load_and_filter_incidence_rdd_file(varicelle_filepath, filter_ile_de_france)
+
+print(gripe_data)
+print(diarrhee_data)
+print(varicelle_data)

c6fb34588a6eace5e57a6c859044f7bb2819b8f2
abouysso <adrien.bouyssou@utbm.fr>
2020-11-12 14:39:50 +0100

    [UPD] Load all CSVs
    1. Update and rename the function load_and_filter_csv (new name of load_and_filter_incidence_rdd_file()):
    	- It take a new param separator to set manually the CSV separator. By default it is at ','. (Update Doctstring accordingly)
    	- It is generic so it can be used with any CSVs, not only for incidence-rdd-... files
    2. Use load_and_filter_csv to load all data files and filter with the date and the geography

--- app.py
+++ app.py
@@ -9,29 +9,45 @@ varicelle_filepath = datadir + 'incidence-RDD-7-varicelle.csv'
 days_off_filepath = datadir + 'hollidays_and_days_off.csv'
 
 
-def load_and_filter_incidence_rdd_file(filepath, row_validator):
+def load_and_filter_csv(filepath, row_validator, separator=','):
     """
-    Load data from a csv incidence-RDD-... (could function with other CSV but the function is written for this files)
+    Load data from a csv
     Filter the got data using the function row_validator
 
     :param filepath: string -  The path to the file containing the wanted data.
     :param row_validator: function - Function returning a Boolean. It will be used to filter which rows will be kept
+    :param separator: str - The separator between each element
 
     :return: A pandas.DataFrame containing the data of filepath filtered by row_validator
     """
-    data = pandas.read_csv(filepath)
-    filtered_data = [row[1] for row in data.iterrows() if row_validator(row)]
-    return pandas.DataFrame(numpy.array(filtered_data), columns= data.columns)
+    data = pandas.read_csv(filepath, sep=separator, skip_blank_lines=True).dropna()
+    columns = data.columns
 
-filter_ile_de_france = lambda row: row[1]['geo_name'] == 'ILE-DE-FRANCE'
-gripe_data = load_and_filter_incidence_rdd_file(gripe_filepath, filter_ile_de_france)
-diarrhee_data = load_and_filter_incidence_rdd_file(diarrhee_filepath, filter_ile_de_france)
-varicelle_data = load_and_filter_incidence_rdd_file(varicelle_filepath, filter_ile_de_france)
+    if row_validator is not None:
+        data = [row[1] for row in data.iterrows() if row_validator(row)]
+        data = pandas.DataFrame(numpy.array(data), columns=columns)
+
+    return data
+
+
+sdis91_data = load_and_filter_csv(sdis91_filepath, None, separator=';')
+
+# #Temporary output
+print(sdis91_data)
+
+filter_rdd_files = lambda row: row[1]['geo_name'] == 'ILE-DE-FRANCE' and\
+                               201001 <= row[1]['week'] <= 201752
+gripe_data = load_and_filter_csv(gripe_filepath, filter_rdd_files)
+diarrhee_data = load_and_filter_csv(diarrhee_filepath, filter_rdd_files)
+varicelle_data = load_and_filter_csv(varicelle_filepath, filter_rdd_files)
 
 # #Temporary output
 print(gripe_data)
 print(diarrhee_data)
 print(varicelle_data)
 
-sdis91_data = pandas.read_csv(sdis91_filepath)
-days_off_data = pandas.read_csv(days_off_filepath)
+days_off_data = load_and_filter_csv(days_off_filepath,
+                                    lambda row:'2010-01-01' <= row[1]['date'] <= '2017-12-12')
+
+# #Temporary output
+print(days_off_data)

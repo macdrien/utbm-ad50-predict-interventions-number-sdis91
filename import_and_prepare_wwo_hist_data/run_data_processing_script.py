924bb0c097a5deb62d7bf89f56b6354f061ddd1f
abouysso <adrien.bouyssou@utbm.fr>
2020-11-13 22:30:48 +0100

    [ADD] variables which will be used to build the final CSV and CSV headers

--- import_and_prepare_wwo_hist_data/run_data_processing_script.py
+++ import_and_prepare_wwo_hist_data/run_data_processing_script.py
@@ -6,3 +6,7 @@ import json
 with open('./import_and_prepare_wwo_hist_data/results.json', 'r') as file:
     file_json = file.read()
 original_data = json.loads(file_json)
+
+csv_separator = ','
+csv_headers = ['week', 'sunrise', 'sunset', 'moonrise', 'moonset', 'moon_phase', 'moon_illumination', 'temperature_max', 'temperature_min', 'temperature_average', 'total_snow_cm', 'sun_hour', 'wind_speed_kmph', 'wind_direction_degree', 'precipitation_mm', 'humidity', 'visibility_km', 'pressure', 'cloudcover', 'heat_index_c', 'dew_point_c', 'wind_chill_c', 'wind_gust_kmph', 'feels_like_c', 'uv_index' 'city']
+csv_lines = []

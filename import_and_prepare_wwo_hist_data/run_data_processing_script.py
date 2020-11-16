f9d528c3d9b3d439b33ed43c2cfdf6b824dd477e
abouysso <adrien.bouyssou@utbm.fr>
2020-11-16 19:02:04 +0100

    [ADD] Take the weeknumber and temperatures min and max for each week

--- import_and_prepare_wwo_hist_data/run_data_processing_script.py
+++ import_and_prepare_wwo_hist_data/run_data_processing_script.py
@@ -1,6 +1,7 @@
 import calendar
 # calendar.weekday() - Will be use to take the day of 2010-01-01
 import json
+from datetime import date
 
 # Read the JSON from the file and convert it into a python list of dictionaries
 with open('./import_and_prepare_wwo_hist_data/results.json', 'r') as file:
@@ -16,10 +17,17 @@ csv_separator = ','
 csv_headers = ['week', 'sunrise', 'sunset', 'moonrise', 'moonset', 'moon_phase', 'moon_illumination', 'temperature_max', 'temperature_min', 'temperature_average', 'total_snow_cm', 'sun_hour', 'wind_speed_kmph', 'wind_direction_degree', 'precipitation_mm', 'humidity', 'visibility_km', 'pressure', 'cloud_cover', 'heat_index_c', 'dew_point_c', 'wind_chill_c', 'wind_gust_kmph', 'feels_like_c', 'uv_index', 'city']
 csv_lines = []
 
-for day in daily_data:
-    print(day['date'])
-"""
-for index in range(3, len(daily_data), 7):
-    day = daily_data[index]['date'].split('-')
-    print('{} - {}'.format(daily_data[index]['date'], calendar.weekday(int(day[0]), int(day[1]), int(day[2]))))
-"""
+for monday_index in range(3, len(daily_data), 7):
+    monday = daily_data[monday_index]
+    csv_line = {}
+
+    # Set the week
+    current_date = monday['date'].split('-')
+    week_number = date(int(current_date[0]), int(current_date[1]), int(current_date[2])).isocalendar()[1]
+    csv_line['week'] = '{}{}'.format(current_date[0], week_number if week_number >= 10 else '0{}'.format(week_number))
+
+    # Get temperature min and max
+    csv_line['temperature_max'] = max([daily_data[monday_index + week_index]['maxtempC'] for week_index in range(7)])
+    csv_line['temperature_min'] = min([daily_data[monday_index + week_index]['mintempC'] for week_index in range(7)])
+
+    csv_lines.append(csv_line)
